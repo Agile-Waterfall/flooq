96795ef822036dc61c22b0c45d67d8a75fbf7eec
Felix Saaro <felix@saaro.ch>
2022-03-09 21:19:13 +0100

    Fix warnings

--- src/api/Flooq.Api/Domain/FlooqContext.cs
+++ src/api/Flooq.Api/Domain/FlooqContext.cs
@@ -7,7 +7,7 @@ namespace Flooq.Domain
     public FlooqContext(DbContextOptions<FlooqContext> options) : base(options)
     { }
 
-    public DbSet<Flooq.Model.DataFlow> DataFlows { get; set; }
-    public DbSet<Flooq.Model.Version> Versions { get; set; }
+    public DbSet<Flooq.Model.DataFlow>? DataFlows { get; set; }
+    public DbSet<Flooq.Model.Version>? Versions { get; set; }
   }
 }
