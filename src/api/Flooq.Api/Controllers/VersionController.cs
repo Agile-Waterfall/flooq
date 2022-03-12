e9a13855954cb2086cf14ac260c2abcf3140300b
Lukas Jans <jansluk@students.zhaw.ch>
2022-03-12 18:03:22 +0100

    #8 Add version endpoint in controller

--- src/api/Flooq.Api/Controllers/VersionController.cs
+++ src/api/Flooq.Api/Controllers/VersionController.cs
@@ -1,15 +1,30 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Flooq.Domain;
 
 namespace Flooq.Controllers
 {
-  [Route("api/[controller]")]
+  [Route("api/version")]
   [ApiController]
   public class VersionController : ControllerBase
   {
-    [HttpGet(Name = "GetVersion")]
-    public string Get()
+    private readonly FlooqContext _context;
+
+    public VersionController(FlooqContext context) {
+      _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<Model.Version>> GetVersion()
     {
-      return "v0.1.1";
+      var version = await _context.Versions.OrderBy(v => v.Tag).LastAsync();
+
+      if (version == null)
+      {
+        return NotFound();
+      }
+
+      return version;
     }
   }
 }
