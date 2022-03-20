3e87838b1416ca9cf53f7dfe451c89e8cb5c43b7
Lukas <helibum1@gmail.com>
2022-03-20 08:32:24 +0100

    Renamed variable for clarity #20

--- src/executor/source/routes/VersionRoute.ts
+++ src/executor/source/routes/VersionRoute.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import { getApiVersion } from '../ApiInterface'
 import Logger from '../utils/logging/Logger'
 
-const router = express.Router()
+const versionRouter = express.Router()
 
-router.get('', async (req, res) => {
+versionRouter.get('', async (req, res) => {
     getApiVersion().then((apiResponse) => {
         res.status(200).send(apiResponse)
     }).catch((reason) => {
@@ -13,4 +13,4 @@ router.get('', async (req, res) => {
     })
 })
 
-export default router;
+export default versionRouter;
