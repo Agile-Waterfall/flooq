c442c82acc236f3e29cd4e2b47f26ffda0f36dee
Silvan Flum <flumsil1@students.zhaw.ch>
2022-04-03 22:45:28 +0200

    Add Demo Data Flow to DB #46

--- src/api/Flooq.Api/Domain/FlooqContext.cs
+++ src/api/Flooq.Api/Domain/FlooqContext.cs
@@ -1,3 +1,4 @@
+using Flooq.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Flooq.Api.Domain
@@ -9,5 +10,16 @@ namespace Flooq.Api.Domain
 
     public DbSet<Models.DataFlow>? DataFlows { get; set; }
     public DbSet<Models.Version>? Versions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      string sampleData;
+      using (StreamReader reader = new StreamReader("sampleDataFlow.json"))
+      {
+        sampleData = reader.ReadToEnd();
+      }
+      modelBuilder.Entity<DataFlow>().HasData(
+        new DataFlow { Id = Guid.NewGuid(), Name = "Demo Flow #1", Status = "Active", LastEdited = new DateTime(2022, 3, 5, 14, 45, 12).ToUniversalTime(), Definition = sampleData});
+    }
   }
 }
