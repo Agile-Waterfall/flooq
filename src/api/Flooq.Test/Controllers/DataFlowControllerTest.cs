fc0087f048d1bac916e16641b7c32bb35a399345
Silvan Flum <flumsil1@students.zhaw.ch>
2022-04-03 23:50:32 +0200

    Create DataFlowControllerTest.cs

---
+++ src/api/Flooq.Test/Controllers/DataFlowControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Flooq.Api.Controllers;
+using Flooq.Api.Domain;
+using Flooq.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flooq.Test.Controllers;
+
+[TestClass]
+public class DataFlowControllerTest
+{
+  private readonly FlooqContext _context = new FlooqContext(new DbContextOptionsBuilder<FlooqContext>().UseInMemoryDatabase("FlooqDatabase").Options);
+  
+  [TestMethod]
+  public void CanGetDemoDataFlow()
+  {
+    var dataFlow = new DataFlow();
+    DataFlowController dataFlowController = new DataFlowController(_context);
+
+    Assert.IsNotNull(dataFlowController.GetDataFlows());
+  }
+}
