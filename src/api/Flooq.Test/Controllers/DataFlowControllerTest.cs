46116cc5580a8b759496fb34c8fc90ea83d71cee
Silvan Flum <flumsil1@students.zhaw.ch>
2022-04-04 17:50:22 +0200

    Add Test Method CanGetDataFlow

--- src/api/Flooq.Test/Controllers/DataFlowControllerTest.cs
+++ src/api/Flooq.Test/Controllers/DataFlowControllerTest.cs
@@ -18,7 +18,7 @@ public class DataFlowControllerTest
   public void CanCreateDataFlowController()
   {
     var mockDataFlowService = _serviceMock.Object;
-    DataFlowController dataFlowController = new DataFlowController(mockDataFlowService);
+    var dataFlowController = new DataFlowController(mockDataFlowService);
     Assert.IsNotNull(dataFlowController);
   }
 
@@ -34,15 +34,35 @@ public class DataFlowControllerTest
       Definition = "{}"
     };
     
-    List<DataFlow> dataFlows = new List<DataFlow>();
+    var dataFlows = new List<DataFlow>();
     dataFlows.Add(demoDataFlow);
     
     var actionResult = new ActionResult<IEnumerable<DataFlow>>(dataFlows);
     _serviceMock.Setup(service => service.GetDataFlows()).ReturnsAsync(actionResult);
 
     var mockDataFlowService = _serviceMock.Object;
-    DataFlowController dataFlowController = new DataFlowController(mockDataFlowService);
+    var dataFlowController = new DataFlowController(mockDataFlowService);
     
     Assert.AreSame(actionResult, dataFlowController.GetDataFlows().Result);
+  }
+
+  [TestMethod]
+  public void CanGetDataFlow()
+  {
+    var demoDataFlow = new DataFlow
+    {
+      Id = Guid.NewGuid(),
+      Name = "Demo Flow",
+      Status = "Active",
+      LastEdited = DateTime.Now,
+      Definition = "{}"
+    };
+
+    _serviceMock.Setup(service => service.GetDataFlow(demoDataFlow.Id)).ReturnsAsync(demoDataFlow);
+
+    var mockDataFlowService = _serviceMock.Object;
+    var dataFlowController = new DataFlowController(mockDataFlowService);
+
+    Assert.AreSame(demoDataFlow, dataFlowController.GetDataFlow(demoDataFlow.Id));
   }
 }
