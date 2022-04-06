c426c86bc3dc8909b59a617bcdd4d3369426e396
Felix Saaro <felix@saaro.ch>
2022-04-06 23:00:28 +0200

    Cleanup list item status

--- src/editor/components/list/list-item-status.tsx
+++ src/editor/components/list/list-item-status.tsx
@@ -1,25 +1,27 @@
-import {CheckCircleIcon, XCircleIcon} from "@heroicons/react/solid";
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/solid'
 
 interface ListItemStatusProps {
   status: string
 }
 
-export const ListItemStatus = ({status}: ListItemStatusProps): JSX.Element => {
-  const getStatusIcon = (status: string) => {
-    switch (status) {
+export const ListItemStatus = ( { status }: ListItemStatusProps ): JSX.Element => {
+  const getStatusIcon = ( status: string ): JSX.Element => {
+    switch ( status ) {
       case 'Active':
-        return <CheckCircleIcon className="text-emerald-400 h-4 w-4"/>
+        return <CheckCircleIcon className="text-emerald-400 h-4 w-4" />
       case 'Disabled':
-        return <XCircleIcon className="text-red-400 h-4 w-4"/>
+        return <XCircleIcon className="text-red-400 h-4 w-4" />
     }
+
+    return <></>
   }
-  
-  const icon = getStatusIcon(status)
+
+  const icon = getStatusIcon( status )
 
   return (
-      <div className="flex items-center">
-        {icon}
-        <div className="m-1 dark:text-gray-100">{status}</div>
-      </div>
+    <div className="flex items-center">
+      {icon}
+      <div className="m-1 dark:text-gray-100">{status}</div>
+    </div>
   )
 }
