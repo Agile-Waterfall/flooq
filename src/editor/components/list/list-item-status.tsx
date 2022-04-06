684e6ad6fd1704ee20dbfbc891a80356e80d65cf
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-04-06 22:56:23 +0200

    add and use list-item-status component

---
+++ src/editor/components/list/list-item-status.tsx
@@ -0,0 +1,25 @@
+import {CheckCircleIcon, XCircleIcon} from "@heroicons/react/solid";
+
+interface ListItemStatusProps {
+  status: string
+}
+
+export const ListItemStatus = ({status}: ListItemStatusProps): JSX.Element => {
+  const getStatusIcon = (status: string) => {
+    switch (status) {
+      case 'Active':
+        return <CheckCircleIcon className="text-emerald-400 h-4 w-4"/>
+      case 'Disabled':
+        return <XCircleIcon className="text-red-400 h-4 w-4"/>
+    }
+  }
+  
+  const icon = getStatusIcon(status)
+
+  return (
+      <div className="flex items-center">
+        {icon}
+        <div className="m-1 dark:text-gray-100">{status}</div>
+      </div>
+  )
+}
