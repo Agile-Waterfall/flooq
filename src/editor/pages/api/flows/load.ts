0ebe825879190bdaf447f952d44ace59876f7c7a
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-04-06 16:14:45 +0200

    rename dataFlow variables

--- src/editor/pages/api/flows/load.ts
+++ src/editor/pages/api/flows/load.ts
@@ -1,16 +1,8 @@
 const handler = async ( _req: any, res: any ): Promise<void> => {
+  const response = await fetch( `${process.env.API_BASE_URL}/api/DataFlow` )
+  const dataFlows = await response.json()
 
-  const url = `${process.env.API_BASE_URL}/api/DataFlow`
-  const request = {
-    headers: {
-      accept: 'text/plain'
-    },
-  }
-
-  const response = await fetch( url, request )
-  const payload = await response.json()
-
-  res.status( response.status ).json( payload )
+  res.status( response.status ).json( dataFlows )
 }
 
 export default handler
