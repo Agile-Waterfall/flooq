070a02405da83ced808fec5b862397343bd81399
Felix Saaro <felix@saaro.ch>
2022-04-07 00:04:24 +0200

    Cleanup delete api handler

--- src/editor/pages/api/flows/delete.ts
+++ src/editor/pages/api/flows/delete.ts
@@ -1,19 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async ( req: NextApiRequest, res: NextApiResponse ): Promise<void> => {
-  try {
-    const response = await fetch( `${process.env.API_BASE_URL}/api/DataFlow/${req.body.id}`, {
-      method: req.method,
-      body: JSON.stringify( req.body ),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    } )
-
-    res.status( response.status ).json( {} )
-  } catch ( e ) {
-    res.status( 500 ).json( { error: 'Failed to save data flow.', exception: e } )
-  }
+  const response = await fetch( `${process.env.API_BASE_URL}/api/DataFlow/${req.body.id}`, {
+    method: req.method,
+    body: JSON.stringify( req.body ),
+    headers: { 'Content-Type': 'application/json' }
+  } )
+  res.status( response.status ).json( {} )
 }
 
 export default handler
