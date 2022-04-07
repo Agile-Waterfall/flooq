1fa857df8ccb3331571db66a3514d09fab21c4cb
Silvan Flum <54156690+silvan-flum@users.noreply.github.com>
2022-04-07 08:53:45 +0200

    Add Doc IDataFlowService.cs

--- src/api/Flooq.Api/Services/IDataFlowService.cs
+++ src/api/Flooq.Api/Services/IDataFlowService.cs
@@ -11,20 +11,60 @@ namespace Flooq.Api.Services;
 public interface IDataFlowService
 { 
   /// <summary>
-  /// Queries the all Data Flows from the Flooq database.
+  /// Queries the all DataFlows from the Flooq database.
   /// </summary>
-  /// <returns>all Data Flows</returns>
+  /// <returns>All DataFlows from the Flooq database.</returns>
   Task<ActionResult<IEnumerable<DataFlow>>> GetDataFlows();
 
+  /// <summary>
+  /// Finds a specific DataFlow in the Flooq database according to a unique identification.
+  /// </summary>
+  /// <returns>The identified DataFlow.</returns>
+  /// <param name="id">The value which is used for identifying the DataFlow.</param>
   Task<ActionResult<DataFlow>> GetDataFlow(Guid? id);
   
+  /// <summary>
+  /// Sets a DataFlow entry according to the given EntityState.
+  /// For example if EntityState.Modified is used, the existing DataFlow entry will be overwritten by the new DataFlow.
+  /// </summary>
+  /// <param name="dataFlow">The new DataFlow.</param>
+  /// <param name="entityState">Defines the action of the new DataFlow entry.</param>
   void SetEntryState(DataFlow dataFlow, EntityState entityState);
-  
-  Task<int> SaveChangesAsync();
 
+  /// <summary>
+  /// Saves all changes made in this context to the database.
+  /// </summary>
+  /// <returns>
+  /// A task that represents the asynchronous save operation.
+  /// The task result contains the number of state entries written to the database.
+  /// </returns>
+  Task<int> SaveChangesAsync();
+  
+  /// <summary>
+  /// Begins tracking the given DataFlow, and any other reachable entities that are not already being tracked,
+  /// in the Added state such that they will be inserted into the database when SaveChanges() is called.
+  /// </summary>
+  /// <param name="dataFlow">The DataFlow to add.</param>
+  /// <returns>
+  /// The EntityEntry<DataFlow> for the entity.
+  /// The entry provides access to change tracking information and operations for the entity.
+  /// </returns>
   public EntityEntry<DataFlow> AddDataFlow(DataFlow dataFlow);
 
+  /// <summary>
+  /// Begins tracking the given DataFlow in the Deleted state such that it will be removed from the database when SaveChanges() is called.
+  /// </summary>
+  /// <param name="dataFlow">The DataFlow to Remove.</param>
+  /// <returns>
+  /// The EntityEntry<DataFlow> for the entity.
+  /// The entry provides access to change tracking information and operations for the entity.
+  /// </returns>
   public EntityEntry<DataFlow> RemoveDataFlow(DataFlow dataFlow);
 
+  /// <summary>
+  /// Checks if the DataFlow with the given identification exists.
+  /// </summary>
+  /// <param name="id">The identification of the DataFlow.</param>
+  /// <returns>true if the DataFlow exists, else false.</returns>
   public bool DataFlowExists(Guid? id);
 }
