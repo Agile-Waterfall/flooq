ae56290c6150142c2ff7dea74f2a21c5efec9983
Silvan Flum <54156690+silvan-flum@users.noreply.github.com>
2022-04-07 08:58:27 +0200

    Clean Code VersionControllerTest.cs

--- src/api/Flooq.Test/Controllers/VersionControllerTest.cs
+++ src/api/Flooq.Test/Controllers/VersionControllerTest.cs
@@ -9,18 +9,12 @@ namespace Flooq.Test.Controllers;
 [TestClass]
 public class VersionControllerTest
 {
-  private Mock<IVersionService> serviceMock;
-
-  [TestInitialize]
-  public void Setup()
-  {
-    serviceMock = new Mock<IVersionService>();
-  }
+  private readonly Mock<IVersionService> _serviceMock = new ();
 
   [TestMethod]
   public void CanCreateVersionController()
   {
-    var mockVersionService = serviceMock.Object;
+    var mockVersionService = _serviceMock.Object;
     VersionController versionController = new VersionController(mockVersionService);
     Assert.IsNotNull(versionController);
   }
@@ -29,9 +23,9 @@ public class VersionControllerTest
   public void CanGetLatestVersion()
   {
     var version = new Version();
-    serviceMock.Setup(service => service.GetLatestVersion()).ReturnsAsync(version);
+    _serviceMock.Setup(service => service.GetLatestVersion()).ReturnsAsync(version);
 
-    var mockVersionService = serviceMock.Object;
+    var mockVersionService = _serviceMock.Object;
     VersionController versionController = new VersionController(mockVersionService);
 
     Assert.AreSame(version, versionController.GetVersion().Result.Value);
@@ -40,9 +34,9 @@ public class VersionControllerTest
   [TestMethod]
   public void CannotGetVersion_WithoutVersionAvailable()
   {
-    serviceMock.Setup(service => service.GetLatestVersion()).ReturnsAsync((Version)null);
+    _serviceMock.Setup(service => service.GetLatestVersion()).ReturnsAsync((Version) null);
 
-    var mockVersionService = serviceMock.Object;
+    var mockVersionService = _serviceMock.Object;
     var versionController = new VersionController(mockVersionService);
     Assert.AreSame(null, versionController.GetVersion().Result.Value);
   }
