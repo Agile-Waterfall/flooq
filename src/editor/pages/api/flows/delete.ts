d083bd7f0e72b0879b43413613107f1be53de5fd
Felix Saaro <felix@saaro.ch>
2022-04-07 09:16:44 +0200

    Explicitly set method

--- src/editor/pages/api/flows/delete.ts
+++ src/editor/pages/api/flows/delete.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async ( req: NextApiRequest, res: NextApiResponse ): Promise<void> => {
   const response = await fetch( `${process.env.API_BASE_URL}/api/DataFlow/${req.body.id}`, {
-    method: req.method,
+    method: 'DELETE',
     body: JSON.stringify( req.body ),
     headers: { 'Content-Type': 'application/json' }
   } )
