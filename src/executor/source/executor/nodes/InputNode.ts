3ac941ccdb18848bdadeb5ee9832a74d3d7e6774
Valentin Huber <contact@valentinhuber.me>
2022-04-08 11:34:36 +0200

    documenting InputNode

--- src/executor/source/executor/nodes/InputNode.ts
+++ src/executor/source/executor/nodes/InputNode.ts
@@ -1,5 +1,9 @@
 import { DataflowInput } from '../../Dataflow'
 
+/**
+ * @param dataflowInput to return
+ * @returns the dataflow input
+ */
 export function executeInputNode( dataflowInput: DataflowInput ): any {
   return dataflowInput
 }
