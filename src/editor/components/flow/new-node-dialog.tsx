a184e15fa2a9d0b8dfdc41016ac5174e99fb5df4
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-04-08 23:53:51 +0200

    made node-types more dynamic

--- src/editor/components/flow/new-node-dialog.tsx
+++ src/editor/components/flow/new-node-dialog.tsx
@@ -1,7 +1,7 @@
 import { Dialog } from '../dialog'
 import { Node as ReactFlowNode } from 'react-flow-renderer/nocss'
 import { NodeTypeOption } from './node-type-option'
-import { FilterNode, HttpInNode, HttpOutNode } from '../graph/node-types'
+import { nodeTypes } from '../graph/node-types'
 
 interface AddNodeDialogProps {
   isAddNodeOpen: boolean,
@@ -33,11 +33,16 @@ export const AddNodeDialog = ( {
       onClose={(): void => setIsAddNodeOpen( false )}
       title={`Add Node to: ${flow.name}`}
     >
-      <hr />
+      <hr/>
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 py-4 dark:text-gray-100">
-        <NodeTypeOption title="HTTP Input" description="A HTTP Input Node, used to get data from a http request. This node can be used as the start Node of your flow." onClick={(): void => addNode( new HttpInNode() )}/>
-        <NodeTypeOption title="HTTP Output" description="A HTTP Output Node, used to send data in the form of a http request. This node can be used as the end Node of your flow." onClick={(): void => addNode( new HttpOutNode() )}/>
-        <NodeTypeOption title="Filter" description="A Filter Node, useful for filtering the data flowing through your data flow. Don't need parts of your incoming data? Just add a Filter Node!" onClick={(): void => addNode( new FilterNode() )}/>
+        {nodeTypes.map( node => (
+          <NodeTypeOption
+            key={node.id}
+            title={node.title}
+            description={node.description}
+            onClick={(): void => addNode( node.create() )}
+          />
+        ) )}
       </div>
     </Dialog>
   )
--- src/editor/components/graph/node-types.ts
+++ src/editor/components/graph/node-types.ts
@@ -1,32 +1,42 @@
 import { Node as ReactFlowNode } from 'react-flow-renderer/dist/nocss/types/nodes'
 import { XYPosition } from 'react-flow-renderer/dist/nocss/types/utils'
+import { v4 as uuidv4 } from 'uuid'
 
-class Node implements ReactFlowNode {
+abstract class Node implements ReactFlowNode {
+  type: string
+  title: string
+  description: string
   id: string
   dragHandle: string
-  type: string
   data: any
   position: XYPosition
 
-  constructor( type: string, data: any ) {
-    this.id = 'id-' + Date.now()
-    this.dragHandle = '.custom-drag-handle'
+  protected constructor( type: string, title: string, description: string, data: any ) {
     this.type = type
+    this.title = title
+    this.description = description
+    this.id = uuidv4()
+    this.dragHandle = '.custom-drag-handle'
     this.data = data
     this.position = this.randomPosition( -100, 100 )
   }
 
-  randomPosition = ( min: number, max: number ): XYPosition => {
+  abstract create(): Node
+
+  private randomPosition = ( min: number, max: number ): XYPosition => {
     return { 'x': this.randomIntFromInterval( min, max ), 'y': this.randomIntFromInterval( min, max ) }
   }
 
-  randomIntFromInterval = ( min: number, max: number ): number => {
+  private randomIntFromInterval = ( min: number, max: number ): number => {
     return Math.floor( Math.random() * ( max - min + 1 ) + min )
   }
 }
 
-export class HttpInNode extends Node {
+class HttpInNode extends Node {
   constructor() {
+    const type = 'httpIn'
+    const title = 'HTTP Input'
+    const description = 'A HTTP Input Node, used to get data from a http request. This node can be used as the start Node of your flow.'
     const data = {
       title: 'http in',
       input: {
@@ -38,12 +48,24 @@ export class HttpInNode extends Node {
       incomingHandles: [],
       outgoingHandles: [{ 'id': 'out', 'name': 'out' }]
     }
-    super( 'httpIn', data )
+    super(
+      type,
+      title,
+      description,
+      data
+    )
+  }
+
+  create = (): HttpInNode => {
+    return new HttpInNode()
   }
 }
 
-export class HttpOutNode extends Node {
+class HttpOutNode extends Node {
   constructor() {
+    const type = 'httpOut'
+    const title = 'HTTP Output'
+    const description = 'A HTTP Output Node, used to send data in the form of a http request. This node can be used as the end Node of your flow.'
     const data = {
       title: 'http out',
       output: {
@@ -55,12 +77,24 @@ export class HttpOutNode extends Node {
       incomingHandles: [{ 'id': 'in', 'name': 'in' }],
       outgoingHandles: []
     }
-    super( 'httpOut', data )
+    super(
+      type,
+      title,
+      description,
+      data
+    )
+  }
+
+  create = (): HttpOutNode => {
+    return new HttpOutNode()
   }
 }
 
-export class FilterNode extends Node {
+class FilterNode extends Node {
   constructor() {
+    const type = 'filter'
+    const title = 'Filter'
+    const description = 'A Filter Node, useful for filtering the data flowing through your data flow. Don\'t need parts of your incoming data? Just add a Filter Node!'
     const data = {
       title: 'Filter',
       filter: {
@@ -71,6 +105,21 @@ export class FilterNode extends Node {
       incomingHandles: [{ 'id': 'in', 'name': 'in' }],
       outgoingHandles: [{ 'id': 'out', 'name': 'out' }]
     }
-    super( 'filter', data )
+    super(
+      type,
+      title,
+      description,
+      data
+    )
+  }
+
+  create = (): FilterNode => {
+    return new FilterNode()
   }
 }
+
+export const nodeTypes = [
+  new HttpInNode(),
+  new HttpOutNode(),
+  new FilterNode()
+]
