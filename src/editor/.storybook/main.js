3e1883f42324b144036c38fb75201e7acd1735ed
Felix Saaro <felix@saaro.ch>
2022-04-08 17:42:16 +0200

    Improve data flow list item story

--- src/editor/.storybook/main.js
+++ src/editor/.storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-postcss",
+    "@storybook/addon-knobs",
     "storybook-addon-sass-postcss",
   ],
   "framework": "@storybook/react"
