f334a86444765a0833e2beb8f77460de0e44be76
Lukas Jans <jansluk@students.zhaw.ch>
2022-04-11 22:05:34 +0200

    Add Flooq.IntegrationTest project in API

---
+++ src/api/Flooq.IntegrationTest/VersionTest.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Flooq.IntegrationTest;
+
+[TestClass]
+public class VersionTest
+{
+  [TestMethod]
+  public void TestMethod1()
+  {
+  }
+}
