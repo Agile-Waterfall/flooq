241fb43dd7a6e9c2edf76b4f9935b47d45bac6db
Lukas Jans <jansluk@students.zhaw.ch>
2022-04-23 22:03:45 +0200

    Add GitHub authentication configuration.

--- src/api/Flooq.Authentication/Program.cs
+++ src/api/Flooq.Authentication/Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
   .AddEntityFrameworkStores<FlooqAuthenticationContext>();
 builder.Services.AddRazorPages();
 
+builder.Services.AddAuthentication(options => { })
+  .AddGitHub(options =>
+  {
+    options.ClientId = "2ff1d7ecd82f0813741c";
+    options.ClientSecret = "ec0597b6bb44488c1030095adb5abd92a6ff098d";
+  });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
