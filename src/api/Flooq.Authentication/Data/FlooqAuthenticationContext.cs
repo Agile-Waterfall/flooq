c1544540f8758aa2896704ace47072c8b042a240
Lukas Jans <jansluk@students.zhaw.ch>
2022-04-23 20:52:31 +0200

    Add Flooq.Authentication project with files generated by code generator.

---
+++ src/api/Flooq.Authentication/Data/FlooqAuthenticationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flooq.Authentication.Data;
+
+public class FlooqAuthenticationContext : IdentityDbContext
+{
+  public FlooqAuthenticationContext(DbContextOptions<FlooqAuthenticationContext> options)
+    : base(options)
+  {
+  }
+}
