f0f50cfb2494d3f9795e87ef1789fa87dc2fbe91
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-04-28 11:50:59 +0200

    add e2e test for Dashboard page

--- src/editor/spec/e2e/pages.js
+++ src/editor/spec/e2e/pages.js
@@ -2,12 +2,28 @@ describe( 'Test for the Pages of flooq.io', function () {
   test( 'Test the Developer page', function ( browser ) {
     browser
       .url( 'https://editor-staging.flooq.io/developer' )
+      .assert.not.urlContains( 'http://' )
       .waitForElementVisible( 'main' )
+      .assert.elementPresent( 'header' )
       .assert.elementPresent( 'main' )
       .assert.titleContains( 'Flooq | Developer' )
+      .assert.textContains( 'header h1', 'Developer' )
+      .assert.textContains( 'main li:nth-child(1)', 'running' )
+      .assert.textContains( 'main li:nth-child(2)', 'connected' )
+      .end()
+  } )
+  test( 'Test the Dashboard page', function ( browser ) {
+    browser
+      .url( 'https://editor-staging.flooq.io/' )
       .assert.not.urlContains( 'http://' )
-      .assert.textContains( 'li:nth-child(1)', 'running' )
-      .assert.textContains( 'li:nth-child(2)', 'connected' )
+      .waitForElementVisible( 'body' )
+      .assert.elementPresent( 'header' )
+      .assert.elementPresent( 'main' )
+      .assert.elementPresent( 'main button' )
+      .assert.titleContains( 'Flooq | Dashboard' )
+      .assert.textContains( 'header h1', 'Dashboard' )
+      .assert.textContains( 'main button', 'Add new Data Flow' )
+
       .end()
   } )
 } )
