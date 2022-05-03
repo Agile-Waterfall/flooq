68640494836bb5494c76997c6b42ac18463bfb46
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-03 14:38:23 +0200

    clean up nightwatch.conf.js

--- src/editor/nightwatch.conf.js
+++ src/editor/nightwatch.conf.js
@@ -1,16 +1,10 @@
 module.exports = {
-  // An array of folders (excluding subfolders) where your tests are located;
-  // if this is not specified, the test source must be passed as the second argument to the test runner.
   src_folders: ['spec/e2e'],
 
   webdriver: {
     start_process: true,
     port: 9515,
     server_path: require( 'chromedriver' ).path,
-    cli_args: [
-      // very verbose geckodriver logs
-      // '-vv'
-    ]
   },
 
   test_settings: {
