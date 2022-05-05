cec3d4f62c698ad95da6b5e587db187ca1a778d7
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-05 14:28:45 +0200

    initial landing page project setup

---
+++ src/landing/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Landing() {
+  return (
+    <>
+      <h1><b>Flooq.io Landing Page</b></h1>
+    </>
+  )
+}
