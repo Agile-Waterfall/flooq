894a37cd93136ae66ca2ed1d8c3c6d28be31b4fd
Valentin Huber <contact@valentinhuber.me>
2022-05-05 13:57:18 +0200

    setting default values for auth client id/secret

--- src/executor/source/request/Authentication.ts
+++ src/executor/source/request/Authentication.ts
@@ -3,18 +3,10 @@ import 'dotenv/config'
 
 let client: BaseClient
 
-if ( !process.env.IDENTITY_SERVER_ISSUER
-  || process.env.IDENTITY_SERVER_ISSUER.length === 0
-  || !process.env.IDENTITY_SERVER_CLIENT_ID
-  || process.env.IDENTITY_SERVER_CLIENT_ID.length === 0
-  || !process.env.IDENTITY_SERVER_CLIENT_SECRET
-  || process.env.IDENTITY_SERVER_CLIENT_SECRET.length === 0 )
-  throw new Error ( 'IDENTITY_SERVER_CLIENT_ID, IDENTITY_SERVER_CLIENT_SECRET or IDENTITY_SERVER_ISSUER not set' )
-
-const issuerURL = process.env.IDENTITY_SERVER_ISSUER
+const issuerURL = process.env.IDENTITY_SERVER_ISSUER || 'DEFAULT_IDENTITY_SERVER_ISSUER'
 const config: ClientMetadata = {
-  client_id: process.env.IDENTITY_SERVER_CLIENT_ID,
-  client_secret: process.env.IDENTITY_SERVER_CLIENT_SECRET,
+  client_id: process.env.IDENTITY_SERVER_CLIENT_ID || 'DEFAULT_CLIENT_ID',
+  client_secret: process.env.IDENTITY_SERVER_CLIENT_SECRET || 'DEFAULT_CLIENT_SECRET',
 }
 
 export async function getJWT(): Promise<string> {
