aa009bd067ee45f6c815fd31dee5ba1d88c6e08f
Valentin Huber <contact@valentinhuber.me>
2022-05-05 13:05:59 +0200

    removing jwt refreshing

--- src/executor/source/request/Authentication.ts
+++ src/executor/source/request/Authentication.ts
@@ -1,36 +1,22 @@
-import { Issuer, TokenSet, BaseClient } from 'openid-client'
+import { Issuer, BaseClient, ClientMetadata } from 'openid-client'
+import 'dotenv/config'
 
-let token: TokenSet
 let client: BaseClient
 
-async function getClient(): Promise<BaseClient> {
-  if ( !client ) {
-    const client_id = process.env.AUTHORIZATION_CLIENT_ID
-    const client_secret = process.env.AUTHORIZATION_CLIENT_SECRET
-    const issuerURL = process.env.IDENTITY_SERVER_ISSUER
+if ( process.env.IDENTITY_SERVER_ISSUER === undefined
+  || process.env.AUTHORIZATION_CLIENT_ID === undefined
+  || process.env.AUTHORIZATION_CLIENT_SECRET === undefined )
+  throw new Error ( 'AUTHORIZATION_CLIENT_ID, AUTHORIZATION_CLIENT_SECRET or IDENTITY_SERVER_ISSUER not set' )
 
-    if ( client_id === undefined || client_secret === undefined || issuerURL === undefined )
-      return Promise.reject(
-        'AUTHORIZATION_CLIENT_ID, AUTHORIZATION_CLIENT_SECRET or IDENTITY_SERVER_ISSUER not set'
-      )
-
-    client = await Issuer.discover( issuerURL ).then( issuer => new issuer.Client( { client_id, client_secret } ) )
-  }
-  return client
+const issuerURL = process.env.IDENTITY_SERVER_ISSUER
+const config: ClientMetadata = {
+  client_id: process.env.AUTHORIZATION_CLIENT_ID,
+  client_secret: process.env.AUTHORIZATION_CLIENT_SECRET,
 }
 
 export async function getJWT(): Promise<string> {
-  if ( !token )
-    token = await getClient().then( c => c.grant( { grant_type: 'client_credentials' } ) )
-
-  if ( token.expired() ) {
-    if ( token.refresh_token ) {
-      token = await getClient().then( c => c.refresh( token ) )
-    } else {
-      token = await getClient().then( c => c.grant( { grant_type: 'client_credentials' } ) )
-    }
-  }
-
+  if ( !client ) client = await Issuer.discover( issuerURL ).then( issuer => new issuer.Client( config ) )
+  const token = await client.grant( { grant_type: 'client_credentials' } )
   if ( !token.access_token ) return Promise.reject( 'Could not retrieve token' )
   return token.access_token
 }
