c88242387e1b9374a0a48da4141c1ce06a9e992f
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-07 16:55:12 +0200

    add first cta prototype

--- src/landing/pages/index.js
+++ src/landing/pages/index.js
@@ -4,12 +4,14 @@ import Image from 'next/image'
 import {Header} from "../components/header/header";
 import Hero from "../components/hero";
 import Feature from "../components/feature";
+import CTA from "../components/cta";
 
 export default function Landing() {
   return (
     <>
       <Hero/>
       <Feature/>
+      <CTA/>
     </> 
   )
 }
