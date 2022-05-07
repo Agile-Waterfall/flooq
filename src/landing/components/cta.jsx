827d43a76ffd5228a3081d12a9dde595112ef3f0
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-07 17:11:27 +0200

    change cta bg-color

--- src/landing/components/cta.jsx
+++ src/landing/components/cta.jsx
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 export default function CTA() {
   return (
-    <div className="bg-blue-50">
+    <div className="bg-gray-100">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-24 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">
           <span className="block">Ready to dive in?</span>
