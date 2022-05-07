c6c86dae73596664a62249805f2e897587c1b2fb
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-07 16:47:42 +0200

    add first feature prototype

--- src/landing/pages/index.js
+++ src/landing/pages/index.js
@@ -3,11 +3,13 @@ import Image from 'next/image'
 
 import {Header} from "../components/header/header";
 import Hero from "../components/hero";
+import Feature from "../components/feature";
 
 export default function Landing() {
   return (
     <>
       <Hero/>
+      <Feature/>
     </> 
   )
 }
