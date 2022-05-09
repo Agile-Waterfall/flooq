d4d24ad5e984053e3f850e3cf024fe748c3e74c9
Felix Saaro <felix@saaro.ch>
2022-05-09 11:17:00 +0200

    Improve data flow list story

--- src/editor/stories/dashboard/DataFlowList.stories.tsx
+++ src/editor/stories/dashboard/DataFlowList.stories.tsx
@@ -1,29 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { object } from '@storybook/addon-knobs'
 import { DataFlowListItem, DataFlowListItemProps } from '../../components/dashboard/data-flow-list-item'
 import { List } from '../../components/list/list'
+import dayjs from 'dayjs'
 
 export default {
   title: 'Molecules/Dashboard',
   component: DataFlowListItem
 } as ComponentMeta<typeof DataFlowListItem>
 
-const Template: ComponentStory<any> = ( args ) => (
-  <div style={{ maxWidth: '1440px' }}>
-    <List
-      title="DataFlows"
-      description="These are the DataFlows you have access to."
-      action={{
-        label: 'Create',
-        onClick: console.log
-      }}
-    >{Object.values<DataFlowListItemProps>( args ).map( ( item: DataFlowListItemProps, i: number ): any => (
-      <DataFlowListItem key={i} {...item} />
-    ) )}
-    </List>
-  </div>
-)
+const Template: ComponentStory<any> = ( args ) => {
+
+  const [items, setItems] = useState( Object.values<DataFlowListItemProps>( args ) )
+  const byLastEdited = ( a: any, b: any ): number => dayjs( b.lastEdited ).diff( dayjs( a.lastEdited ), 'seconds' )
+
+  return (
+    <div style={{ maxWidth: '1440px' }}>
+      <List
+        title="DataFlows"
+        description="These are the DataFlows you have access to."
+        action={{
+          label: 'Create',
+          onClick: () => setItems( [...items, { id: items.length, name: `New Item #${items.length}`, lastEdited: new Date().toISOString(), status: 'Disabled' }] )
+        }}
+      >{items.sort( byLastEdited )?.map( ( item: DataFlowListItemProps, i: number ): any => (
+          <DataFlowListItem key={i} {...item} />
+        ) )}
+      </List>
+    </div>
+  )
+}
 
 export const DataFlowList = Template.bind( {} )
 
