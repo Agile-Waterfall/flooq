e97ad72864048e9f813a2ce35bcb632254ba8c89
Felix Saaro <felix@saaro.ch>
2022-05-11 14:13:45 +0200

    Show redirect message if user is signed in already

--- src/editor/pages/login.tsx
+++ src/editor/pages/login.tsx
@@ -1,14 +1,17 @@
 import { NextPage } from 'next'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 const LogoutDone: NextPage<void> = () => {
   const session = useSession()
+  const router = useRouter()
   useEffect( () => {
-    console.log( session )
     if ( !session || session.status === 'unauthenticated' ) {
       signIn( 'flooq', { callbackUrl: '/' } )
+    } else {
+      router.push( '/' )
     }
   } )
 
@@ -19,9 +22,16 @@ const LogoutDone: NextPage<void> = () => {
       </Head>
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-            You will be redirected to the sign in page in a moment.
-          </p>
+          {!session || session.status === 'unauthenticated' &&
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
+              You will be redirected to the sign in page in a moment.
+            </p>
+          }
+          {session && session.status === 'authenticated' &&
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
+              You will be redirected to the dashboard in a moment.
+            </p>
+          }
         </div>
       </main>
     </>
