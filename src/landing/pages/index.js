a75a0bbf54cbc253d38cccad1a3c594d54c5b514
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-11 10:26:19 +0200

    add basic faq component

--- src/landing/pages/index.js
+++ src/landing/pages/index.js
@@ -3,15 +3,16 @@ import Feature from "../components/feature";
 import CTA from "../components/cta";
 import Pricing from "../components/pricing";
 import Footer from "../components/footer";
+import FAQ from "../components/faq";
 
 export default function Landing() {
   return (
     <>
-      
       <Hero/>
       <Feature/>
       <CTA/>
       <Pricing/>
+      <FAQ/>
       <Footer/>
     </> 
   )
