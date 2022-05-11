ded1ea640a4ce0a406a4bfe09064cfbd8f330b8a
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-11 13:16:14 +0200

    add editor url as public env variable

--- src/landing/components/cta.jsx
+++ src/landing/components/cta.jsx
@@ -9,7 +9,7 @@ export default function CTA() {
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
             <a
-              href="https://editor-staging.flooq.io/"
+              href={process.env.NE}
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md bg-blue-500 hover:bg-blue-400 text-gray-100"
             >
               Sign up
--- src/landing/components/hero.jsx
+++ src/landing/components/hero.jsx
@@ -18,7 +18,7 @@ export default function Hero() {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="https://editor-staging.flooq.io/"
+                    href={process.env.NEXT_PUBLIC_EDITOR_URL}
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-400 text-gray-100 md:py-4 md:text-lg md:px-10"
                   >
                     Get started
