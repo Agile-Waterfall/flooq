7270349ce5ad086f1cca73fec61d93727a4c0236
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-11 21:24:23 +0200

    dark mode for hero

--- src/landing/components/hero.jsx
+++ src/landing/components/hero.jsx
@@ -2,17 +2,19 @@ import HeroImage from "./heroImage";
 
 export default function Hero() {
   return (
-    <div className="relative overflow-hidden mt-16">
+    <div className="relative overflow-hidden bg-white dark:bg-gray-800 mt-16">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-gray-50 sm:text-5xl md:text-6xl">
+              <h1
+                className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-gray-50 sm:text-5xl md:text-6xl">
                 <span className="block">Data</span>
                 <span className="block">Transformation</span>
                 <span className="block text-amber-400">made easy</span>
               </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              <p
+                className="mt-3 text-base text-gray-500 dark:text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Connect applications quickly and with ease.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
