9367d57e6e3474cee85c4410d4ca8542489ae0c6
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-12 10:43:03 +0200

    initial mail list form

--- src/landing/components/contact.jsx
+++ src/landing/components/contact.jsx
@@ -5,26 +5,7 @@ export default function Contact() {
     <div id="contact" className="bg-white dark:bg-gray-800">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <div className="max-w-lg mx-auto md:max-w-none md:grid md:grid-cols-2 md:gap-8">
-          <div>
-            <h2 className="text-2xl font-extrabold text-gray-900 dark:text-white sm:text-3xl">Where we are</h2>
-            <div className="mt-3">
-              <p className="text-lg text-gray-500 dark:text-gray-400">
-                We are located in the beautiful swiss city of Winterthur. Although or team consists of people from many different places. 
-              </p>
-            </div>
-            <div className="mt-9">
-              <div className="flex">
-                <div className="flex-shrink-0">
-                  <LibraryIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
-                </div>
-                <div className="ml-3 text-base text-gray-500 dark:text-gray-400">
-                  <p>Agile Waterfall inc.</p>
-                  <p className="mt-1">Gertrudstrasse 15</p>
-                  <p className="mt-1">8400 Winterthur</p>
-                </div>
-              </div>
-            </div>
-          </div>
+
           <div className="mt-12 sm:mt-16 md:mt-0">
             <h2 className="text-2xl font-extrabold text-gray-900 dark:text-white sm:text-3xl">Contact us</h2>
             <div className="mt-3">
@@ -43,6 +24,38 @@ export default function Contact() {
               </div>
             </div>
           </div>
+          
+          <div className="mt-12 sm:mt-16 md:mt-0">
+            <h2 className="text-2xl font-extrabold text-gray-900 dark:text-white sm:text-3xl">E-mail signup</h2>
+            <div className="mt-3">
+              <p className="text-lg text-gray-500 dark:text-gray-400">
+                Do you want to receive information about new developments or features? Sign up for our newsletter today.
+              </p>
+            </div>
+            <div className="mt-9">
+              <form className="mt-5 sm:flex sm:items-center">
+                <div className="w-full sm:max-w-xs">
+                  <label htmlFor="email" className="sr-only">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    placeholder="you@example.com"
+                  />
+                </div>
+                <button 
+                  type="submit"
+                  className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  Save
+                </button>
+              </form>
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
