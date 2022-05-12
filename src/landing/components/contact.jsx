fbcfa327c609146d1923a3b215a8bab3b0aef5f6
Niklas van der Heide <vandenik@students.zhaw.ch>
2022-05-12 11:17:07 +0200

    move mail signup to own component

--- src/landing/components/contact.jsx
+++ src/landing/components/contact.jsx
@@ -1,11 +1,11 @@
-import { BellIcon, LibraryIcon, MailIcon, MailOpenIcon, PhoneIcon } from '@heroicons/react/outline'
+import { MailIcon } from '@heroicons/react/outline'
+import Signup from "./signup";
 
 export default function Contact() {
   return (
     <div id="contact" className="bg-white dark:bg-gray-800">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <div className="max-w-lg mx-auto md:max-w-none md:grid md:grid-cols-2 md:gap-8">
-
           <div className="mt-12 sm:mt-16 md:mt-0">
             <h2 className="text-2xl font-extrabold text-gray-900 dark:text-white sm:text-3xl">Contact us</h2>
             <div className="mt-3">
@@ -24,44 +24,7 @@ export default function Contact() {
               </div>
             </div>
           </div>
-
-          <div className="mt-12 sm:mt-16 md:mt-0">
-            <h2 className="text-2xl font-extrabold text-gray-900 dark:text-white sm:text-3xl">E-mail signup</h2>
-            <div className="mt-3">
-              <p className="text-lg text-gray-500 dark:text-gray-400">
-                Do you want to receive information about new developments or features? Sign up for our newsletter today.
-              </p>
-            </div>
-            <div className="mt-9">
-              <form className="mt-5 sm:flex sm:items-center">
-                <div className="w-full sm:max-w-xs">
-                  <label htmlFor="email" className="sr-only">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="you@example.com"
-                    className="
-                    p-2 border-2 rounded-md w-full
-                    bg-white dark:bg-gray-900
-                    text-gray-900 dark:text-gray-100 text-sm
-                    disabled:text-gray-400 disabled:bg-gray-200 disabled:border-gray-200
-                    disabled:dark:text-gray-500 disabled:dark:bg-gray-700 disabled:dark:border-gray-700"
-                  />
-                </div>
-                <button
-                  type="submit"
-                    className="ml-2 bg-blue-500 inline-flex items-center justify-center p-2 px-4 rounded-md text-gray-100 hover:text-gray-50 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                  >
-                    <BellIcon className="block h-5 w-5" aria-hidden="true" />
-                    <span className="pl-1">Notify Me</span>
-                  </button>
-              </form>
-            </div>
-          </div>
-
+          <Signup/>
         </div>
       </div>
     </div>
