961984765d0f444d387d7b456e8e26ca28c23c89
Felix Saaro <felix@saaro.ch>
2022-05-17 10:07:17 +0200

    Add delete token modal

--- src/editor/components/actions/delete-token-action.tsx
+++ src/editor/components/actions/delete-token-action.tsx
@@ -1,30 +1,30 @@
 import { Dialog } from '@headlessui/react'
 import { Button } from '../form/button'
 
-interface DeleteAccountActionProps {
+interface DeleteTokenActionProps {
   open: boolean,
   setOpen: ( open: boolean ) => void
   onDelete: () => void
 }
 
-export const DeleteAccountAction = ( { open, setOpen, onDelete }: DeleteAccountActionProps ): JSX.Element => (
+export const DeleteTokenAction = ( { open, setOpen, onDelete }: DeleteTokenActionProps ): JSX.Element => (
   <Dialog open={open} onClose={(): void => setOpen( false )}>
     <div className="fixed w-full h-full full-height z-20 top-0 left-0 flex items-center justify-center bg-gray-100 dark:bg-gray-800 bg-opacity-50 dark:bg-opacity-70">
       <div className="bg-white dark:bg-gray-900 shadow sm:rounded-lg z-30 relative">
         <div className="px-4 py-5 sm:p-6">
 
           <Dialog.Title className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-50">
-            Delete your account
+            Delete Token
           </Dialog.Title>
           <Dialog.Description className="mt-2 max-w-xl text-sm text-gray-500">
-            Once you delete your account, you will lose all data associated with it.
+            Once you delete this token, you will will not be able to recover it.
           </Dialog.Description>
           <div className="mt-5 flex gap-4">
             <Button
               dangerous
               onClick={onDelete}
             >
-              Delete Account
+              Delete Token
             </Button>
             <Button
               secondary
